0d435eae631f32751e7d22476084332875c6f7cc
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2020-04-14 13:01:28 +0200

    add results

---
+++ pixelart/zoom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+EXEC=/Users/lachaud/GITHUB/misc/DGtal-CGAL/build/tv-triangulation-color
+ZOOM="$1"
+INPUT="input/*.png"
+OUTDIR="output-x${ZOOM}"
+
+for i in ${INPUT}; do
+    echo "------- $i ---- x ${ZOOM} -------"
+    BASE=`echo $i | sed 's/.png//' | sed 's/input\///'`
+    IFILE="${BASE}.ppm"
+    OFILE="${OUTDIR}/${BASE}"
+    convert "${i}" "${IFILE}"
+    if test ! -d ${OUTDIR}; then
+	mkdir ${OUTDIR}
+    fi
+    ${EXEC} -b ${ZOOM} -D 16 -i "${IFILE}" -S 1 -o ${OFILE}
+    rm ${IFILE}
+done
